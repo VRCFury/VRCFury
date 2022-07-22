c4651302a28dd33a39b98d5875269c85e75cf06e
Senky <11907914-senky@users.noreply.gitlab.com>
2022-07-22 12:29:22 -0500

    Improve outer bound calculation

--- Editor/VF/Feature/BoundingBoxFixBuilder.cs
+++ Editor/VF/Feature/BoundingBoxFixBuilder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -16,23 +17,22 @@ namespace VF.Feature {
             var skins = avatarObject.GetComponentsInChildren<SkinnedMeshRenderer>(true);
 
             foreach (var skin in skins) {
-                var worldMin = skin.transform.TransformPoint(skin.localBounds.min);
-                var worldMax = skin.transform.TransformPoint(skin.localBounds.max);
+                if (skin.rootBone == null) continue;
                 if (first) {
                     first = false;
-                    overallWorldBounds.SetMinMax(worldMin, worldMax);
+                    overallWorldBounds.SetMinMax(skin.bounds.min, skin.bounds.max);
                 } else {
-                    overallWorldBounds.Encapsulate(worldMin);
-                    overallWorldBounds.Encapsulate(worldMax);
+                    overallWorldBounds.Encapsulate(skin.bounds.min);
+                    overallWorldBounds.Encapsulate(skin.bounds.max);
                 }
             }
 
             foreach (var skin in skins) {
-                var localBounds = new Bounds();
-                localBounds.SetMinMax(
-                    skin.transform.InverseTransformPoint(overallWorldBounds.min),
-                    skin.transform.InverseTransformPoint(overallWorldBounds.max)
-                );
+                if (skin.rootBone == null) continue;
+                var localBounds = skin.localBounds;
+                foreach (var corner in GetCorners(overallWorldBounds)) {
+                    localBounds.Encapsulate(skin.rootBone.InverseTransformPoint(corner));
+                }
                 skin.localBounds = localBounds;
             }
         }
@@ -54,6 +54,19 @@ namespace VF.Feature {
                 }
             });
             return content;
+        }
+        
+        private static List<Vector3> GetCorners(Bounds obj, bool includePosition = true)
+        {
+            var result = new List<Vector3>();
+            for (int x = -1; x <= 1; x += 2)
+            for (int y = -1; y <= 1; y += 2)
+            for (int z = -1; z <= 1; z += 2) {
+                var offset = (obj.size / 2);
+                offset.Scale(new Vector3(x, y, z));
+                result.Add((includePosition ? obj.center : Vector3.zero) + offset);
+            }
+            return result;
         }
     }
 }
