466f28d4753f95314c9e0bf94139d3e974f2f0e7
Senky <11907914-senky@users.noreply.gitlab.com>
2023-01-02 10:58:58 -0600

    Use disabled or missing fingers first for global colliders

--- Editor/VF/Feature/BakeGlobalContactsBuilder.cs
+++ Editor/VF/Feature/BakeGlobalContactsBuilder.cs
@@ -10,17 +10,39 @@ namespace VF.Feature {
     public class BakeGlobalContactsBuilder : FeatureBuilder {
         [FeatureBuilderAction(FeatureOrder.BakeOgbComponents)]
         public void Apply() {
+
             var globalContacts = avatarObject.GetComponentsInChildren<VRCFGlobalContactSender>(true);
             var avatar = avatarObject.GetComponent<VRCAvatarDescriptor>();
 
-            var fingers = new List<(VRCAvatarDescriptor.ColliderConfig, Action<VRCAvatarDescriptor.ColliderConfig>)> {
-                ( avatar.collider_fingerLittleL, c => avatar.collider_fingerLittleL = c ),
-                ( avatar.collider_fingerLittleR, c => avatar.collider_fingerLittleR = c ),
-                ( avatar.collider_fingerRingL, c => avatar.collider_fingerRingL = c ),
-                ( avatar.collider_fingerRingR, c => avatar.collider_fingerRingR = c ),
-                ( avatar.collider_fingerMiddleL, c => avatar.collider_fingerMiddleL = c ),
-                ( avatar.collider_fingerMiddleR, c => avatar.collider_fingerMiddleR = c ),
+            var fingers = new List<(HumanBodyBones, VRCAvatarDescriptor.ColliderConfig, Action<VRCAvatarDescriptor.ColliderConfig>)> {
+                ( HumanBodyBones.LeftLittleIntermediate, avatar.collider_fingerLittleL, c => avatar.collider_fingerLittleL = c ),
+                ( HumanBodyBones.RightLittleIntermediate, avatar.collider_fingerLittleR, c => avatar.collider_fingerLittleR = c ),
+                ( HumanBodyBones.LeftRingIntermediate, avatar.collider_fingerRingL, c => avatar.collider_fingerRingL = c ),
+                ( HumanBodyBones.RightRingIntermediate, avatar.collider_fingerRingR, c => avatar.collider_fingerRingR = c ),
+                ( HumanBodyBones.LeftMiddleIntermediate, avatar.collider_fingerMiddleL, c => avatar.collider_fingerMiddleL = c ),
+                ( HumanBodyBones.RightMiddleIntermediate, avatar.collider_fingerMiddleR, c => avatar.collider_fingerMiddleR = c ),
             };
+            
+            // Put unused fingers on the front of the list
+            {
+                var unused = new List<(HumanBodyBones, VRCAvatarDescriptor.ColliderConfig, Action<VRCAvatarDescriptor.ColliderConfig>)>();
+                var used = new List<(HumanBodyBones, VRCAvatarDescriptor.ColliderConfig, Action<VRCAvatarDescriptor.ColliderConfig>)>();
+                while (fingers.Count >= 2) {
+                    var left = fingers[0];
+                    var right = fingers[1];
+                    fingers.RemoveRange(0, 2);
+                    if (!IsFingerUsed(left.Item1, left.Item2) && (left.Item2.isMirrored || !IsFingerUsed(right.Item1, right.Item2))) {
+                        unused.Add(left);
+                        unused.Add(right);
+                    } else {
+                        used.Add(left);
+                        used.Add(right);
+                    }
+                }
+                fingers.Clear();
+                fingers.AddRange(unused);
+                fingers.AddRange(used);
+            }
             
             if (globalContacts.Length > fingers.Count) {
                 throw new VRCFBuilderException("Too many VRCF global contacts are present on this avatar");
@@ -28,8 +50,8 @@ namespace VF.Feature {
 
             var i = 0;
             foreach (var globalContact in globalContacts) {
-                var finger = fingers[i].Item1;
-                var setFinger = fingers[i].Item2;
+                var finger = fingers[i].Item2;
+                var setFinger = fingers[i].Item3;
                 finger.isMirrored = false;
                 finger.state = VRCAvatarDescriptor.ColliderConfig.State.Custom;
                 // We can't make this height customizable because vrchat does its own weird calculations at runtime,
@@ -48,12 +70,19 @@ namespace VF.Feature {
             }
             if (i % 2 == 1) {
                 // If an odd number, disable the matching mirrored finger
-                var finger = fingers[i].Item1;
-                var setFinger = fingers[i].Item2;
+                var finger = fingers[i].Item2;
+                var setFinger = fingers[i].Item3;
                 finger.isMirrored = false;
                 finger.state = VRCAvatarDescriptor.ColliderConfig.State.Disabled;
                 setFinger(finger);
             }
+        }
+
+        private bool IsFingerUsed(HumanBodyBones bone, VRCAvatarDescriptor.ColliderConfig config) {
+            var animator = avatarObject.GetComponent<Animator>();
+            if (config.state == VRCAvatarDescriptor.ColliderConfig.State.Disabled) return false;
+            if (animator.GetBoneTransform(bone) == null) return false;
+            return true;
         }
     }
 }
