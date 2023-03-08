3a822679f21f1091a6ec1ccdc436fdf1158325ea
Senky <11907914-senky@users.noreply.gitlab.com>
2023-03-08 17:09:26 -0600

    initial release test

--- .github/workflows/updateUrls.js
+++ .github/workflows/updateUrls.js
@@ -7,9 +7,9 @@ for (const asset of assetJson) {
     const m = asset.name.match(/^([^-]+)-(.+)\.tgz$/);
     if (!m) throw new Error(`Asset name didn't match pattern: ${asset.name}`);
     const name = m[1];
-    const version = [2];
+    const version = m[2];
     const entry = versionJson.packages.find(v => v.id === name && v.latestVersion === version);
-    if (!entry) throw new Error(`Asset didn't match an entry in the version manifest: ${name} ${version} ${versionJson}`);
+    if (!entry) throw new Error(`Asset didn't match an entry in the version manifest: ${name} ${version}`);
     entry.latestUpmTargz = asset.browser_download_url;
 }
 
