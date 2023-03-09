1c0159e07e3fc97f1a25d3a2e7086fb668681957
Senky <11907914-senky@users.noreply.gitlab.com>
2023-03-09 06:47:53 -0600

    Exclude asmdef and json from installer

--- .github/workflows/build_unitypackage.sh
+++ .github/workflows/build_unitypackage.sh
@@ -22,8 +22,12 @@ function make_meta_directory() {
 
     echo "Adding $asset_file to $path_prefix/$asset_file"
     guid=$(yq e '.guid' "$meta_file")
+    # we reverse all the guids so they don't match the ones in the upm upgrade package
+    guid=$(echo "$guid" | rev)
     dir="$tmp_dir/$guid"
     mkdir $dir
+    cp "$meta_file" "$dir/asset.meta"
+    yq e -i ".guid = \"$guid\"" "$dir/asset.meta"
     echo "$path_prefix/$asset_file" > $dir/pathname
     if [[ -f "$asset_file" ]]; then
       cp "$asset_file" "$dir/asset"
