6b7af8fb9815c78aa4e295c00aabaeab6f57f738
Senky <11907914-senky@users.noreply.gitlab.com>
2023-03-14 04:06:00 -0500

    Don't conflict ListInstalledPackages

--- com.vrcfury.updater/VF/Updater/VRCFuryUpdaterStartup.cs
+++ com.vrcfury.updater/VF/Updater/VRCFuryUpdaterStartup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -26,6 +27,10 @@ namespace VF.Updater {
         
         private static readonly bool IsRunningInsidePackage =
             Assembly.GetExecutingAssembly().GetName().Name == "VRCFury-Updater2";
+
+        private static readonly bool UpdatePackageExists =
+            AppDomain.CurrentDomain.GetAssemblies()
+                .Any(assembly => assembly.GetName().Name == "VRCFury-Updater2");
         private static void DebugLog(string msg) {
             var suffix = IsRunningInsidePackage ? "" : " (installer)";
             Debug.Log($"VRCFury Updater{suffix}: {msg}");
@@ -41,8 +46,10 @@ namespace VF.Updater {
 
             var actions = new PackageActions();
 
-            var packages = await PackageActions.ListInstalledPacakges();
-            if (!packages.Any(p => p.name == "com.vrcfury.updater")) {
+            // Don't call ListInstalledPackages here, since if we do it at the same time as another assembly,
+            // it will fail to complete one of the calls
+
+            if (!UpdatePackageExists) {
                 // Updater package (... this package) isn't installed, which means this code
                 // is probably running inside of the standalone installer, and we need to go install
                 // the updater and main vrcfury package.
