ff16ea2f5fa9df5b544a288b8f6cf980998d5e93
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-06-02 22:36:00 -0500

    Enforce hasExitTime = false by default for users that have plugins that change the default

--- com.vrcfury.vrcfury/Editor/VF/Builder/VRCFuryAnimatorUtils.cs
+++ com.vrcfury.vrcfury/Editor/VF/Builder/VRCFuryAnimatorUtils.cs
@@ -331,6 +331,7 @@ public class VFATransition {
              var trans = transitionProvider();
              trans.duration = 0;
              trans.canTransitionToSelf = false;
+             trans.hasExitTime = false;
              createdTransitions.Add(trans);
              return trans;
         };
