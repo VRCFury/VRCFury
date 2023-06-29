a2a5f6b1bf23ea85ffc45821e7dff217a9d2df60
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-06-28 21:54:44 -0500

    Totally change assembly name of newtonsoft json to avoid conflicts with vrcsdk's copy

--- com.vrcfury.vrcfury/JsonComponent/Runtime/VF/JsonComponent/JsonComponent.cs
+++ com.vrcfury.vrcfury/JsonComponent/Runtime/VF/JsonComponent/JsonComponent.cs
@@ -6,10 +6,13 @@ namespace VF.Component {
      * This is a component that stores everything as JSON so we don't have to deal with unity's crappy serialization.
      * See JsonComponentExtensions for more details.
      */
+    [ExecuteInEditMode]
     public abstract class JsonComponent : MonoBehaviour {
         public string json;
 
         [NonSerialized] public static Action<JsonComponent> onValidate;
         public void OnValidate() { onValidate?.Invoke(this); }
+        [NonSerialized] public static Action<JsonComponent> onDestroy;
+        public void OnDestroy() { onDestroy?.Invoke(this); }
     }
 }
