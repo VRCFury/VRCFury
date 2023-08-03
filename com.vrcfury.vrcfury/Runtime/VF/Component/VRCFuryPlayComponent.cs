306a3092faf51a7b06f4a3ceb67e6996de3301be
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-08-02 21:42:41 -0500

    Prevent Av3emulator from enabling UpdateWhenOffscreen on SPS meshes

--- com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryPlayComponent.cs
+++ com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryPlayComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace VF.Component {
@@ -10,5 +11,13 @@ namespace VF.Component {
         public Vector3 pos;
         public Quaternion rot;
         public bool show = true;
+    }
+
+    public class VRCFuryNoUpdateWhenOffscreen : VRCFuryPlayComponent {
+        private void Update() {
+            var skin = GetComponent<SkinnedMeshRenderer>();
+            if (skin == null) return;
+            skin.updateWhenOffscreen = false;
+        }
     }
 }
