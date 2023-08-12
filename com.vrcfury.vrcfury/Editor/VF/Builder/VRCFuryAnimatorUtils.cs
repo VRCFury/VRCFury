32817fb2ea40fc7e57dcd82950726bb766c3e00e
Matthew Kuhr <kuhrm9095@gmail.com>
2023-08-12 13:15:25 -0400

    revert unnecessary change

--- com.vrcfury.vrcfury/Editor/VF/Builder/VRCFuryAnimatorUtils.cs
+++ com.vrcfury.vrcfury/Editor/VF/Builder/VRCFuryAnimatorUtils.cs
@@ -423,8 +423,8 @@ public class VFATransition {
         };
     }
     public VFATransition When() {
-            _ = transitionProvider();
-            return this;
+        var transition = transitionProvider();
+        return this;
     }
     public VFATransition When(VFACondition cond) {
         foreach (var t in cond.transitions) {
