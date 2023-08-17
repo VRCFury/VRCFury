1fb6682c2896b5d925af849460d685f57e811e8f
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-08-17 02:35:38 -0500

    Verify id is not null before upgrading in GuideWrapper

--- com.vrcfury.vrcfury/Runtime/VF/Model/GuidWrapper.cs
+++ com.vrcfury.vrcfury/Runtime/VF/Model/GuidWrapper.cs
@@ -66,7 +66,7 @@ namespace VF.Model {
             if (fromVersion < 1) {
                 if (guid != "") id = guid + ":" + fileID;
             }
-            if (!id.Contains("|")) {
+            if (id != null && !id.Contains("|")) {
                 // Old ID where we didn't save the filename
                 // Try to upgrade it, just in case
                 TryToAddNames?.Invoke(this);
