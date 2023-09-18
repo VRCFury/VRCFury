587f7144a63b9017213ba97f6099ff69042c9b69
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-09-17 22:04:29 -0500

    test

--- .github/workflows/test_ci.sh
+++ .github/workflows/test_ci.sh
@@ -11,18 +11,11 @@ unity-editor \
 
 platforms=(editmode playmode)
 
-COVERAGE_OPTIONS="pathFilters:+/**/Assets/Scripts/**;assemblyFilters:+assets;generateAdditionalMetrics;generateHtmlReport;generateBadgeReport"
-
 for platform in "${platforms[@]}"; do
     unity-editor \
         -runTests \
         -projectPath /opt/project/.github/workflows/test_project \
-        -testResults /opt/project/Logs/${platform}_test_results.xml \
         -logfile /dev/stdout \
-        -debugCodeOptimization \
-        -enableCodeCoverage \
         -testPlatform $platform \
-        -coverageResultsPath /opt/project/CodeCoverage \
-        -coverageOptions $COVERAGE_OPTIONS \
-        -runSynchronously
+        -quit
 done
