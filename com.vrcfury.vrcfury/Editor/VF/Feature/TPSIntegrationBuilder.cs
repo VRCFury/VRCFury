153c2ac30f1a9955114a72b5d7c599210c0a1f01
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-11-17 18:23:22 -0600

    Update old TPS warnings

--- com.vrcfury.vrcfury/Editor/VF/Feature/TPSIntegrationBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/TPSIntegrationBuilder.cs
@@ -21,7 +21,7 @@ namespace VF.Feature {
         
         public override VisualElement CreateEditor(SerializedProperty prop) {
             var content = new VisualElement();
-            content.Add(VRCFuryEditorUtils.Error("This feature is deprecated and now does nothing. Use VRCFury Haptic Socket and VRCFury Haptic Plug (with TPS autoconfiguration) instead!"));
+            content.Add(VRCFuryEditorUtils.Error("This feature is deprecated and now does nothing. Use SPS instead! See vrcfury.com/sps"));
             return content;
         }
 
