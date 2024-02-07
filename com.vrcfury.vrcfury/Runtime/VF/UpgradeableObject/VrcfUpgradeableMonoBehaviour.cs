bfb1dcd4636cde8447b62c655e96229b7a938e0d
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-02-07 09:03:10 -0600

    Revert backup saving

--- com.vrcfury.vrcfury/Runtime/VF/UpgradeableObject/VrcfUpgradeableMonoBehaviour.cs
+++ com.vrcfury.vrcfury/Runtime/VF/UpgradeableObject/VrcfUpgradeableMonoBehaviour.cs
@@ -1,5 +1,3 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 
 namespace VF.Upgradeable {
@@ -7,34 +5,15 @@ namespace VF.Upgradeable {
         [SerializeField] private int version = -1;
         public string unityVersion;
         public string vrcfuryVersion;
-        public string backup;
-        [NonSerialized] private bool backupLoaded;
 
         public static string currentVrcfVersion { private get; set; }
 
         public int Version { get => version; set => version = value; }
-
-        void ISerializationCallbackReceiver.OnAfterDeserialize() {
-            this.IUpgradeableOnAfterDeserialize();
-        }
-
-        public void OnValidate() {
-            //Debug.Log("VALIDATE");
-            if (!backupLoaded) {
-                backupLoaded = true;
-                //Debug.Log("Loading backup " + backup);
-                EditorJsonUtility.FromJsonOverwrite(backup, this);
-            } else {
-                unityVersion = Application.unityVersion;
-                vrcfuryVersion = currentVrcfVersion;
-                backup = null;
-                backup = EditorJsonUtility.ToJson(this);
-                //Debug.Log("Saved backup " + backup);
-                EditorUtility.SetDirty(this);
-            }
-        }
+        void ISerializationCallbackReceiver.OnAfterDeserialize() { this.IUpgradeableOnAfterDeserialize(); }
 
         void ISerializationCallbackReceiver.OnBeforeSerialize() {
+            unityVersion = Application.unityVersion;
+            vrcfuryVersion = currentVrcfVersion;
             this.IUpgradeableOnBeforeSerialize();
         }
 
