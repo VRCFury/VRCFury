81c62d335cd4e46dfbf4f26de227083e352787b0
sentfromspacevr <146104647+sentfromspacevr@users.noreply.github.com>
2024-02-07 01:29:47 +0100

    Revert back to accumulating diff

--- com.vrcfury.vrcfury/Editor/VF/Feature/RadialToggleOptimizerBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/RadialToggleOptimizerBuilder.cs
@@ -37,12 +37,12 @@ namespace Editor.VF.Feature {
             var pointer = fx.NewInt("SyncPointer", synced: true);
             var localPointer = fx.NewInt("LocalPointer"); // Use a local only pointer to avoid race condition
             var data = fx.NewFloat("SyncData", synced: true);
-            //var sending = fx.NewFloat("IsSending");
+            var sending = fx.NewFloat("IsSending");
 
             var layer = fx.NewLayer("Radial Toggle Optimizer");
             var idle = layer.NewState("Idle");
-            //txIdle.Drives(sending, 0, true);
             var local = layer.NewState("Local");
+            local.Drives(sending, 0, true);
             var remote = layer.NewState("Remote");
             idle.TransitionsTo(local)
                 .When(fx.IsLocal().IsTrue());
@@ -59,56 +59,46 @@ namespace Editor.VF.Feature {
                 .TransitionsToExit()
                 .When(fx.Always());
 
-            var sending = toOptimize.Select((_, i) => fx.NewFloat($"sending{i}")).ToList();
             // instant sync on change detection
             for (int i = 0; i < toOptimize.Count; i++) {
                 var src = toOptimize[i];
                 var lastSrc = math.Buffer(src);
                 var srcDiff = math.Subtract(src, lastSrc);
-                
                 var pending = math.SetValueWithConditions($"diffPending{i}",
-                    (0, math.GreaterThan(sending[i], 0.5f)),
-                    (1, math.Or(math.GreaterThan(srcDiff, 0), math.LessThan(srcDiff, 0))));
-                /*var maintain = math.MakeMaintainer(srcDiff); // Maintain while in send animation
-                directTree.Add(sending, maintain);*/
-                var sendInstantPrepareState = layer.NewState($"Send Prepare Instant {toOptimize[i].Name()}");
-                sendInstantPrepareState.Drives(sending[i], 1, true);
+                    (1, math.Or(math.GreaterThan(srcDiff, 0), math.LessThan(srcDiff, 0))),
+                    (0, null));
+                var maintain = math.MakeMaintainer(pending); // Maintain while in send animation
+                directTree.Add(sending, maintain);
+
                 var sendInstantState = layer.NewState($"Send Instant {toOptimize[i].Name()}");
                 sendInstantState
                     .DrivesCopy(data, src, true)
                     .Drives(pointer, i, true)
-                    .Drives(sending[i], 0, true)
+                    .Drives(sending, 1, true)
                     .TransitionsToExit()
-                    .WithTransitionExitTime(0.1f)
+                    .WithTransitionExitTime(2f)
                     .When(fx.Always());
-                local.TransitionsTo(sendInstantPrepareState)
-                    .When(pending.IsGreaterThan(0.5f)); // srcDiff.IsGreaterThan(0).Or(srcDiff.IsLessThan(0))));
-                sendInstantPrepareState.TransitionsTo(sendInstantState)
-                    .When(fx.Always());
-                //txIdle.Drives(sending[i], 0, true);
+                local.TransitionsTo(sendInstantState)
+                    .When(pending.IsGreaterThan(0.5f));
+
             }
 
             // round robin pointer iteration
             for (int i = 0; i < toOptimize.Count; i++) {
                 var src = toOptimize[i];
-                var sendPrepareState = layer.NewState($"Send Prepare {toOptimize[i].Name()}");
-                sendPrepareState.Drives(sending[i], 1, true);
                 var sendState = layer.NewState($"Send {toOptimize[i].Name()}");
                 sendState
                     .DrivesCopy(data, src)
                     .DrivesCopy(pointer, localPointer)
-                    .Drives(sending[i], 0, true)
+                    .Drives(sending, 1, true)
                     .TransitionsTo(txAdder)
-                    .WithTransitionExitTime(0.1f)
+                    .WithTransitionExitTime(2f)
                     .When(fx.Always());
-                local.TransitionsTo(sendPrepareState)
-                    .When(fx.IsLocal().IsTrue().And(localPointer.IsEqualTo(i)));
-                sendPrepareState.TransitionsTo(sendState)
-                    .When(fx.Always());
+                local.TransitionsTo(sendState)
+                    .When(localPointer.IsEqualTo(i));
+
             }
             
-            //var rxLayer = fx.NewLayer("Receive");
-            //var rxIdle = rxLayer.NewState("Idle");
             var smoothedDict = new Dictionary<string, string>();
             for (int i = 0; i < toOptimize.Count; i++) {
                 var dst = toOptimize[i];
