27a2f3f68c3244c250d3d5a17592ccd294f86017
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-02-09 07:22:15 -0600

    Fix VRCSDK showing wrong validation info when user clicks android tab on a texture

---
+++ com.vrcfury.vrcfury/Editor/VF/Hooks/FixVrcsdkValidatorWrongPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+using VRC.SDK3A.Editor;
+
+namespace VF.Hooks {
+    /**
+     * The vrcsdk internally uses EditorUserBuildSettings.selectedBuildTargetGroup,
+     * which changes unexpectedly when you select platform options on a texture asset.
+     * They should have used EditorUserBuildSettings.activeBuildTarget.
+     * We fix this by correcting the build target group to match whenever the vrcsdk dialog opens or
+     * a build begins.
+     */
+    public static class FixVrcsdkValidatorWrongPlatform {
+        [InitializeOnLoadMethod]
+        public static void Init() {
+            SyncBuildTargetGroup();
+#if VRC_NEW_PUBLIC_SDK
+            VRCSdkControlPanel.OnSdkPanelEnable += (sender, e) => {
+                SyncBuildTargetGroup();
+                if (VRCSdkControlPanel.TryGetBuilder<IVRCSdkAvatarBuilderApi>(out var builder)) {
+                    builder.OnSdkBuildStart += (sender2, target) => {
+                        SyncBuildTargetGroup();
+                    };
+                }
+            };
+#endif
+        }
+
+        private static void SyncBuildTargetGroup() {
+            EditorUserBuildSettings.selectedBuildTargetGroup =
+                BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+        }
+    }
+}
