6fecfc8668c75480c4b40736f0db88287ecd8539
pi <stefan@pimaker.at>
2024-03-24 21:13:36 +0100

    refactor, optimize, apply review comments

--- com.vrcfury.vrcfury/Editor/VF/Feature/RemoveBlendshapeVerticesBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/RemoveBlendshapeVerticesBuilder.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
-using VF.Builder;
+using VF.Builder.Exceptions;
 using VF.Feature.Base;
 using VF.Inspector;
 using VF.Model.Feature;
@@ -21,76 +23,70 @@ public class RemoveBlendshapeVerticesBuilder : FeatureBuilder<RemoveBlendshapeVe
         }
 
         foreach (var renderer in renderers) {
-            var original = renderer.sharedMesh;
-            if (original == null)
-                continue;
-            var blendshapeIndex = original.GetBlendShapeIndex(model.blendshape);
-            if (blendshapeIndex < 0)
-                continue;
+            var mesh = renderer.sharedMesh;
+            if (mesh == null) continue;
 
-            var mesh = MutableManager.MakeMutable(original, forceCopy: true);
+            var blendshapeIndex = mesh.GetBlendShapeIndex(model.blendshape);
+            if (blendshapeIndex < 0) continue;
+
+            // collect vertices affected by given blendshape, make sure we use `Vector3.Equals` as `Vector3.operator==` uses a delta
             var vertexBuffer = new Vector3[mesh.vertexCount];
             mesh.GetBlendShapeFrameVertices(blendshapeIndex, 0, vertexBuffer, null, null);
             var toRemove = vertexBuffer.Select(v => !v.Equals(Vector3.zero)).ToArray();
-            try
-            {
-                RemoveVerticesFromMesh(toRemove, original, mesh);
-                renderer.sharedMesh = mesh;
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogException(ex);
-            }
+
+            var newMesh = CloneMeshWithRemovedVertices(mesh, toRemove);
+            renderer.sharedMesh = newMesh;
         }
     }
 
     // adapted from: https://github.com/euan142/LazyOptimiser/blob/main/Packages/net.euan.lazyoptimiser/Editor/MeshUtil.cs
-    private static void RemoveVerticesFromMesh(bool[] verticesToRemove, Mesh original, Mesh mesh) {
-        var changeMap = new Dictionary<int, int>();
+    private static Mesh CloneMeshWithRemovedVertices(Mesh original, bool[] verticesToRemove) {
+        var indexChangeMap = new Dictionary<int, int>();
         var newIndex = -1;
-        for (int i = 0; i < original.vertexCount; i++)
-        {
-            if (verticesToRemove[i])
-            {
-                changeMap[i] = -1;
-            }
-            else
-            {
+        for (int i = 0; i < original.vertexCount; i++) {
+            if (verticesToRemove[i]) {
+                indexChangeMap[i] = -1;
+            } else {
                 newIndex++;
-                changeMap[i] = newIndex;
+                indexChangeMap[i] = newIndex;
             }
         }
 
-        mesh.Clear();
-        mesh.ClearBlendShapes();
+        // make a new mesh and copy all data except the vertices we want to remove
+        var mesh = new Mesh();
 
         mesh.SetVertices(original.vertices.Where((_, i) => verticesToRemove[i] == false).ToArray());
-        mesh.SetNormals(original.normals.Where((_, i) => verticesToRemove[i] == false).ToArray());
-        mesh.SetTangents(original.tangents.Where((_, i) => verticesToRemove[i] == false).ToArray());
+        if (original.HasVertexAttribute(VertexAttribute.Normal))
+            mesh.SetNormals(original.normals.Where((_, i) => verticesToRemove[i] == false).ToArray());
+        if (original.HasVertexAttribute(VertexAttribute.Tangent))
+            mesh.SetTangents(original.tangents.Where((_, i) => verticesToRemove[i] == false).ToArray());
+        if (original.HasVertexAttribute(VertexAttribute.Color))
+            mesh.SetColors(original.colors.Where((_, i) => verticesToRemove[i] == false).ToArray());
 
-        mesh.SetColors(original.colors.Where((_, i) => verticesToRemove[i] == false).ToArray());
+        mesh.bindposes = original.bindposes;
         mesh.boneWeights = original.boneWeights.Where((_, i) => verticesToRemove[i] == false).ToArray();
 
-        for (int i = 0; i < 8; i++)
-        {
-            if (mesh.HasVertexAttribute(UnityEngine.Rendering.VertexAttribute.TexCoord0 + i)) {
-                var uvList = new List<Vector4>(original.vertexCount);
+        var uvList = new List<Vector4>(original.vertexCount);
+        for (int i = 0; i < 8; i++) {
+            if (mesh.HasVertexAttribute(VertexAttribute.TexCoord0 + i)) {
                 original.GetUVs(i, uvList);
                 mesh.SetUVs(i, uvList.Where((_, j) => verticesToRemove[j] == false).ToList());
             }
         }
 
-        for (int subMeshNr = 0; subMeshNr < mesh.subMeshCount; subMeshNr++)
-        {
-            int[] subMesh = original.GetTriangles(subMeshNr);
-            List<int> newTris = new List<int>();
-            for (int i = 0; i < subMesh.Length; i += 3)
-            {
-                int t1 = changeMap[subMesh[i]];
-                int t2 = changeMap[subMesh[i+1]];
-                int t3 = changeMap[subMesh[i+2]];
-                if (t1 != -1 && t2 != -1 && t3 != -1)
-                {
+        for (int subMeshNr = 0; subMeshNr < mesh.subMeshCount; subMeshNr++) {
+            var topology = original.GetTopology(subMeshNr);
+            if (topology != MeshTopology.Triangles) {
+                throw new VRCFBuilderException("Only triangle meshes are supported for RemoveBlendshapeVertices. " +
+                    $"Mesh '{original.name}', submesh {subMeshNr}, has topology {topology}.");
+            }
+            var subMesh = original.GetTriangles(subMeshNr);
+            var newTris = new List<int>();
+            for (int i = 0; i < subMesh.Length; i += 3) {
+                var t1 = indexChangeMap[subMesh[i]];
+                var t2 = indexChangeMap[subMesh[i+1]];
+                var t3 = indexChangeMap[subMesh[i+2]];
+                if (t1 != -1 && t2 != -1 && t3 != -1) {
                     newTris.Add(t1);
                     newTris.Add(t2);
                     newTris.Add(t3);
@@ -102,12 +98,10 @@ public class RemoveBlendshapeVerticesBuilder : FeatureBuilder<RemoveBlendshapeVe
         var deltaVertices = new Vector3[original.vertexCount];
         var deltaNormals = new Vector3[original.vertexCount];
         var deltaTangents = new Vector3[original.vertexCount];
-        for (int i = 0; i < original.blendShapeCount; i++)
-        {
+        for (int i = 0; i < original.blendShapeCount; i++) {
             var name = original.GetBlendShapeName(i);
             var weightCount = original.GetBlendShapeFrameCount(i);
-            for (int j = 0; j < weightCount; j++)
-            {
+            for (int j = 0; j < weightCount; j++) {
                 var weight = original.GetBlendShapeFrameWeight(i, j);
                 original.GetBlendShapeFrameVertices(i, j, deltaVertices, deltaNormals, deltaTangents);
                 var newDeltaVertices = deltaVertices.Where((_, k) => verticesToRemove[k] == false).ToArray();
@@ -116,17 +110,17 @@ public class RemoveBlendshapeVerticesBuilder : FeatureBuilder<RemoveBlendshapeVe
                 mesh.AddBlendShapeFrame(name, weight, newDeltaVertices, newDeltaNormals, newDeltaTangents);
             }
         }
+
+        mesh.Optimize();
+        return mesh;
     }
 
-    private int CountAffectedVertices(SkinnedMeshRenderer smr, string blendshapeName) {
-        if (smr == null)
-            return 0;
+    private static int CountAffectedVertices([CanBeNull] SkinnedMeshRenderer smr, string blendshapeName) {
+        if (smr == null) return 0;
         var mesh = smr.sharedMesh;
-        if (mesh == null)
-            return 0;
+        if (mesh == null) return 0;
         var blendshapeIndex = mesh.GetBlendShapeIndex(blendshapeName);
-        if (blendshapeIndex < 0)
-            return 0;
+        if (blendshapeIndex < 0) return 0;
         var vertexBuffer = new Vector3[mesh.vertexCount];
         mesh.GetBlendShapeFrameVertices(blendshapeIndex, 0, vertexBuffer, null, null);
         return vertexBuffer.Count(v => !v.Equals(Vector3.zero));
@@ -134,10 +128,6 @@ public class RemoveBlendshapeVerticesBuilder : FeatureBuilder<RemoveBlendshapeVe
 
     public override string GetEditorTitle() {
         return "Remove Blendshape Vertices";
-    }
-    
-    public override bool AvailableOnRootOnly() {
-        return false;
     }
 
     public override VisualElement CreateEditor(SerializedProperty prop) {
@@ -153,10 +143,12 @@ public class RemoveBlendshapeVerticesBuilder : FeatureBuilder<RemoveBlendshapeVe
 
         content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
                 var all = allRenderersProp.boolValue;
-                var smrs = all ? avatarObject.GetComponentsInSelfAndChildren<SkinnedMeshRenderer>() : new[] {rendererProp.objectReferenceValue as SkinnedMeshRenderer};
-                var count = smrs.Sum(smr => CountAffectedVertices(smr, blendshapeProp.stringValue));
+                var blendshape = blendshapeProp.stringValue;
+                var count = all ?
+                    avatarObject.GetComponentsInSelfAndChildren<SkinnedMeshRenderer>()
+                        .Sum(smr => CountAffectedVertices(smr, blendshape)) :
+                    CountAffectedVertices(rendererProp.objectReferenceValue as SkinnedMeshRenderer, blendshape);
                 var vertexCountLabel = VRCFuryEditorUtils.Info($"Will remove {count} vertices");
-                vertexCountLabel.visible = count > 0;
                 return vertexCountLabel;
             },
             allRenderersProp,
