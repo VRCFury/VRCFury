207f5508cda2e171501a26578f0adf37231d1ef3
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-05-29 10:44:17 -0500

    Initial version of public API

--- com.vrcfury.vrcfury/PublicApi/FuryComponents.cs
+++ com.vrcfury.vrcfury/PublicApi/FuryComponents.cs
@@ -2,9 +2,14 @@
 using UnityEngine;
 
 namespace com.vrcfury.api {
+    /** Public API for creating VRCFury components */
     public static class FuryComponents {
         public static FuryArmatureLink CreateArmatureLink(GameObject obj) {
             return new FuryArmatureLink(obj);
+        }
+        
+        public static FuryFullController CreateFullController(GameObject obj) {
+            return new FuryFullController(obj);
         }
     }
 }
