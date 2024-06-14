19747a5c2054b378ea6a8733dbefda8b5e492f4e
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-06-14 08:34:10 -0500

    Fix component debug info breaking editor when there's an error in the update method

--- com.vrcfury.vrcfury/Editor/VF/Feature/DescriptorDebugBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/DescriptorDebugBuilder.cs
@@ -21,26 +21,24 @@ namespace VF.Feature {
                 "This component displays VRCFury's useful debug info about the controllers on your avatar's base avatar descriptor." +
                     " This component has no impact on the avatar, and changes nothing about the upload. Any detected warnings will be shown below."
                 ));
-            var debugInfo = new VisualElement();
-            output.Add(debugInfo);
-
-            void Update() {
-                debugInfo.Clear();
-                if (avatarObject == null) return;
+            
+            output.Add(VRCFuryEditorUtils.Debug(refreshElement: () => {
+                var debugInfo = new VisualElement();
+                if (avatarObject == null) return debugInfo;
                 var avatar = avatarObject.GetComponent<VRCAvatarDescriptor>();
-                if (avatar == null) return;
+                if (avatar == null) return debugInfo;
 
                 var controllers = VRCAvatarUtils.GetAllControllers(avatar)
                     .Select(c => c.controller as AnimatorController)
                     .NotNull()
                     .ToList();
                 var warnings = VrcfAnimationDebugInfo.BuildDebugInfo(controllers, avatarObject, avatarObject);
+                
                 foreach (var w in warnings) {
                     debugInfo.Add(w);
                 }
-            }
-            Update();
-            debugInfo.schedule.Execute(Update).Every(1000);
+                return debugInfo;
+            }));
 
             return output;
         }
