6a5433357753445be957d588212cb151f22299d9
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-06-22 13:23:43 -0500

    Fix move menu item being in a submenu

--- com.vrcfury.vrcfury/Editor/VF/Feature/MoveMenuItemBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/MoveMenuItemBuilder.cs
@@ -8,7 +8,7 @@ using VF.Model.Feature;
 namespace VF.Feature {
     internal class MoveMenuItemBuilder : FeatureBuilder<MoveMenuItem> {
         public override string GetEditorTitle() {
-            return "Move / Rename Menu Item";
+            return "Move or Rename Menu Item";
         }
         
         public override VisualElement CreateEditor(SerializedProperty prop) {
