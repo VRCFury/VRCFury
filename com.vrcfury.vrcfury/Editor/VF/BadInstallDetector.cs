5f8314c7fba20cdf7cd8649430eff37eb54148bd
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-08-08 01:06:05 -0500

    Don't show update menu item if installed using VCC

--- com.vrcfury.vrcfury/Editor/VF/BadInstallDetector.cs
+++ com.vrcfury.vrcfury/Editor/VF/BadInstallDetector.cs
@@ -7,21 +7,23 @@ namespace VF {
     internal static class BadInstallDetector {
         [InitializeOnLoadMethod]
         private static void Init() {
-            if (!UpdateMenuItem.IsVrcfuryALocalPackage()) return;
+            var isLocalPackage = Directory.Exists("Packages/com.vrcfury.vrcfury") &&
+                                 Path.GetFullPath("Packages/com.vrcfury.vrcfury").StartsWith(Path.GetFullPath("Packages"));
             var manifestPath = "Packages/manifest.json";
-            if (!File.Exists(manifestPath)) return;
-            var manifestContainsVrcfury = File.ReadLines(manifestPath)
+            var manifestContainsVrcfury = File.Exists(manifestPath) && File.ReadLines(manifestPath)
                 .Any(line => line.Contains("com.vrcfury.vrcfury"));
-            if (!manifestContainsVrcfury) return;
-            EditorUtility.DisplayDialog(
-                "VRCFury",
-                "The VRCFury install is partially corrupt. The updater may have broken, or you may have updated " +
-                "from an old manual install to a new version using the VCC.\n" +
-                "\n" +
-                "Please download and import " +
-                "https://vrcfury.com/installer to resolve this issue.",
-                "Ok"
-            );
+
+            if (isLocalPackage && manifestContainsVrcfury) {
+                EditorUtility.DisplayDialog(
+                    "VRCFury",
+                    "The VRCFury install is partially corrupt. The updater may have broken, or you may have updated " +
+                    "from an old manual install to a new version using the VCC.\n" +
+                    "\n" +
+                    "Please download and import " +
+                    "https://vrcfury.com/installer to resolve this issue.",
+                    "Ok"
+                );
+            }
         }
     }
 }
