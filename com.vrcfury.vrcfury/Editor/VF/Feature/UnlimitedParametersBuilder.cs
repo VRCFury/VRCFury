7a8e4e46b281b3a06ffb291202a80d75c5e45654
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-09-27 18:05:56 -0500

    Add warnings for bool and puppet compressors

--- com.vrcfury.vrcfury/Editor/VF/Feature/UnlimitedParametersBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/UnlimitedParametersBuilder.cs
@@ -224,9 +224,27 @@ namespace VF.Feature {
                 "This component will optimize all synced float parameters used in radial menu toggles into 16 total bits"));
             content.Add(VRCFuryEditorUtils.Warn(
                 "This feature is in BETA - Please report any issues on the VRCFury discord"));
-            
-            content.Add(VRCFuryEditorUtils.Prop(prop.FindPropertyRelative("includeBools"), "Optimize Bools"));
-            content.Add(VRCFuryEditorUtils.Prop(prop.FindPropertyRelative("includePuppets"), "Optimize Puppets"));
+
+            var includeBoolsProp = prop.FindPropertyRelative("includeBools");
+            content.Add(VRCFuryEditorUtils.Prop(includeBoolsProp, "Optimize Bools"));
+            content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
+                if (includeBoolsProp.boolValue) {
+                    return VRCFuryEditorUtils.Warn(
+                        "Warning: Compressing bools often doesn't save much space, and can, in some rare cases, cause unusual sync issues with complex avatar systems.");
+                }
+                return new VisualElement();
+            }, includeBoolsProp));
+
+            var includePuppetsProp = prop.FindPropertyRelative("includePuppets");
+            content.Add(VRCFuryEditorUtils.Prop(includePuppetsProp, "Optimize Puppets"));
+            content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
+                if (includePuppetsProp.boolValue) {
+                    return VRCFuryEditorUtils.Warn(
+                        "Warning: Compressing puppets may cause them to not move as smoothly for remote clients as you control them.");
+                }
+                return new VisualElement();
+            }, includePuppetsProp));
+
             return content;
         }
     }
