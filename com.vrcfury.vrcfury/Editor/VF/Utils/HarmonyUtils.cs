eedd8ea7364d65e8f186ccb9f2498dcf34b72353
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-09-28 20:42:35 -0500

    Fix VRCSDK causing extra Animator windows to appear

---
+++ com.vrcfury.vrcfury/Editor/VF/Utils/HarmonyUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+
+namespace VF.Utils {
+    internal static class HarmonyUtils {
+        private static readonly Type harmonyType = ReflectionUtils.GetTypeFromAnyAssembly("HarmonyLib.Harmony");
+        private static readonly MethodInfo harmonyPatch = harmonyType?.GetMethod("Patch");
+        private static readonly Type harmonyMethodType = ReflectionUtils.GetTypeFromAnyAssembly("HarmonyLib.HarmonyMethod");
+        private static readonly ConstructorInfo harmonyMethodConstructor = harmonyMethodType?.GetConstructor(new Type[] { typeof(MethodInfo) });
+
+        private static readonly Lazy<object> harmony = new Lazy<object>(() => {
+            var constructor = harmonyType.GetConstructor(new Type[] { typeof(string) });
+            if (constructor == null) return null;
+            var unpatchAll = harmonyType.GetMethod("UnpatchAll", BindingFlags.Instance | BindingFlags.Public);
+            if (unpatchAll == null) return null;
+            var harmonyInst = constructor.Invoke(new object[] { "com.vrcfury.harmony" });
+            AssemblyReloadEvents.beforeAssemblyReload += () => {
+                ReflectionUtils.CallWithOptionalParams(unpatchAll, harmonyInst);
+            };
+            return harmonyInst;
+        });
+
+        private static object GetHarmony() {
+            return harmony.Value;
+        }
+
+        public static void Patch(MethodInfo methodToPatch, MethodInfo prefix) {
+            if (methodToPatch == null || prefix == null) return;
+            var harmonyInst = GetHarmony();
+            if (harmonyInst == null) return;
+            if (harmonyPatch == null) return;
+            if (harmonyMethodConstructor == null) return;
+            var harmonyMethod = harmonyMethodConstructor.Invoke(new object[] { prefix });
+            ReflectionUtils.CallWithOptionalParams(harmonyPatch, harmonyInst, methodToPatch, harmonyMethod);
+        }
+    }
+}
