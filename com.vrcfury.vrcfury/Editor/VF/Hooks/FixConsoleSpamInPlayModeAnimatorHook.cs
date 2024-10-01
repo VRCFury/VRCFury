918290d4fe4a49671630461dc30a7abc41a02e91
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-10-01 06:19:38 -0500

    Fix unity absolutely spamming the console when you open FX controller while in play mode

---
+++ com.vrcfury.vrcfury/Editor/VF/Hooks/FixConsoleSpamInPlayModeAnimatorHook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEditor;
+using UnityEngine;
+using VF.Utils;
+
+namespace VF.Hooks {
+    /**
+     * If you open an Animator window, targeting an Animator on a gameobject using a mixer (like... an avatar using Gesture Manager / av3emu)
+     * and then you open any controller aside from the first, it will try to incorrectly get the layer weights from the first controller in the mixer,
+     * absolutely SPAMMING the console with tons of warnings every frame. The only 'fix' for this is for us to override
+     * Animator.GetLayerWeight and just make it always return 0.
+     *
+     * Unfortunately:
+     * 1. This patch can not be un-done without restarting unity, because a bug in Harmony prevents Unpatch from working properly on extern methods
+     * 2. You cannot use Harmony Prefix on a extern method, so we must instead substitute its opcodes entirely using those
+     *    from a stub method that looks the same.
+     *
+     * Luckily, basically nobody else in the world uses Animator.GetLayerWeight, especially in avatar projects, because it's
+     * essentially always wrong when the avatar mixer is involved.
+     */
+    internal static class FixConsoleSpamInPlayModeAnimatorHook {
+        [InitializeOnLoadMethod]
+        private static void Init() {
+            if (HarmonyUtils.GetOriginalInstructions == null) return;
+
+            var methodToPatch = typeof(Animator).GetMethod(
+                nameof(Animator.GetLayerWeight),
+                new[] { typeof(int) }
+            );
+
+            var transpiler = typeof(FixConsoleSpamInPlayModeAnimatorHook).GetMethod( 
+                nameof(Transpile),
+                BindingFlags.Static | BindingFlags.NonPublic
+            );
+
+            HarmonyUtils.Transpile(methodToPatch, transpiler);
+        }
+
+        static object Transpile(IEnumerable<object> orig, ILGenerator ilGenerator) {
+            var replacementMethod = typeof(Shim).GetMethod(
+                nameof(Shim.Replacement),
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public
+            );
+            var replacementInstructions = HarmonyUtils.GetOriginalInstructions.Invoke(null, new object[] { replacementMethod, ilGenerator });
+            return replacementInstructions;
+        }
+   
+        class Shim {
+            public float Replacement(int layerIndex) {
+                return 0; 
+            }
+        } 
+    }
+}
--- com.vrcfury.vrcfury/Editor/VF/Utils/HarmonyUtils.cs
+++ com.vrcfury.vrcfury/Editor/VF/Utils/HarmonyUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEditor;
 
 namespace VF.Utils {
@@ -9,17 +10,29 @@ namespace VF.Utils {
         private static readonly Type harmonyMethodType = ReflectionUtils.GetTypeFromAnyAssembly("HarmonyLib.HarmonyMethod");
         private static readonly ConstructorInfo harmonyMethodConstructor = harmonyMethodType?.GetConstructor(new Type[] { typeof(MethodInfo) });
 
-        private static readonly Lazy<object> harmony = new Lazy<object>(() => {
+        public static readonly MethodInfo GetOriginalInstructions = ReflectionUtils
+            .GetTypeFromAnyAssembly("HarmonyLib.PatchProcessor")?.GetMethod(
+                "GetOriginalInstructions",
+                BindingFlags.Public | BindingFlags.Static,
+                null,
+                new[] { typeof(MethodBase), typeof(ILGenerator) },
+                null);
+
+        private static readonly Lazy<object> harmony = new Lazy<object>(() => MakeHarmony(true));
+
+        private static object MakeHarmony(bool unpatchLater = false) {
             var constructor = harmonyType.GetConstructor(new Type[] { typeof(string) });
             if (constructor == null) return null;
             var unpatchAll = harmonyType.GetMethod("UnpatchAll", BindingFlags.Instance | BindingFlags.Public);
             if (unpatchAll == null) return null;
-            var harmonyInst = constructor.Invoke(new object[] { "com.vrcfury.harmony" });
-            AssemblyReloadEvents.beforeAssemblyReload += () => {
-                ReflectionUtils.CallWithOptionalParams(unpatchAll, harmonyInst);
-            };
+            var harmonyInst = constructor.Invoke(new object[] { unpatchLater ? "com.vrcfury.harmony" : "com.vrcfury.harmony.donotunpatch" });
+            if (unpatchLater) {
+                AssemblyReloadEvents.beforeAssemblyReload += () => {
+                    ReflectionUtils.CallWithOptionalParams(unpatchAll, harmonyInst, "com.vrcfury.harmony");
+                };
+            }
             return harmonyInst;
-        });
+        }
 
         private static object GetHarmony() {
             return harmony.Value;
@@ -33,6 +46,16 @@ namespace VF.Utils {
             if (harmonyMethodConstructor == null) return;
             var harmonyMethod = harmonyMethodConstructor.Invoke(new object[] { prefix });
             ReflectionUtils.CallWithOptionalParams(harmonyPatch, harmonyInst, methodToPatch, harmonyMethod);
+        }
+        
+        public static void Transpile(MethodBase methodToPatch, MethodInfo transpiler) {
+            if (methodToPatch == null || transpiler == null) return;
+            var harmonyInst = MakeHarmony(); // We make a fresh harmony, because we can't unpatch these
+            if (harmonyInst == null) return;
+            if (harmonyPatch == null) return;
+            if (harmonyMethodConstructor == null) return;
+            var harmonyMethod = harmonyMethodConstructor.Invoke(new object[] { transpiler });
+            ReflectionUtils.CallWithOptionalParams(harmonyPatch, harmonyInst, methodToPatch, null, null, harmonyMethod);
         }
     }
 }
