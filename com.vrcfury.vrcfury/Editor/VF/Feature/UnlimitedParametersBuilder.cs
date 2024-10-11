83cab0c5c1d9449e7469cd1d562aa216e7209f2f
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-10-10 19:24:11 -0500

    Add more info to parameter compressor component

--- com.vrcfury.vrcfury/Editor/VF/Feature/UnlimitedParametersBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/UnlimitedParametersBuilder.cs
@@ -3,6 +3,7 @@ using UnityEngine.UIElements;
 using VF.Feature.Base;
 using VF.Inspector;
 using VF.Model.Feature;
+using Toggle = UnityEngine.UIElements.Toggle;
 
 namespace VF.Feature {
     [FeatureAlias("Unlimited Parameters")]
@@ -17,19 +18,17 @@ namespace VF.Feature {
                 "This component will optimize all synced float parameters used in radial menu toggles into 16 total bits"));
             content.Add(VRCFuryEditorUtils.Warn(
                 "This feature is in BETA - Please report any issues on the VRCFury discord"));
-
-            var includeBoolsProp = prop.FindPropertyRelative("includeBools");
-            content.Add(VRCFuryEditorUtils.Prop(includeBoolsProp, "Optimize Bools"));
-            content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
-                if (includeBoolsProp.boolValue) {
-                    return VRCFuryEditorUtils.Warn(
-                        "Warning: Compressing bools often doesn't save much space, and can, in some rare cases, cause unusual sync issues with complex avatar systems.");
-                }
-                return new VisualElement();
-            }, includeBoolsProp));
+            
+            var checkedBox2 = new Toggle() { value = true };
+            checkedBox2.SetEnabled(false);
+            content.Add(VRCFuryEditorUtils.Prop(null, "Compress Int/Float Toggles", fieldOverride: checkedBox2));
+            
+            var checkedBox = new Toggle() { value = true };
+            checkedBox.SetEnabled(false);
+            content.Add(VRCFuryEditorUtils.Prop(null, "Compress Radials", fieldOverride: checkedBox));
 
             var includePuppetsProp = prop.FindPropertyRelative("includePuppets");
-            content.Add(VRCFuryEditorUtils.Prop(includePuppetsProp, "Optimize Puppets"));
+            content.Add(VRCFuryEditorUtils.Prop(includePuppetsProp, "Compress Puppets"));
             content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
                 if (includePuppetsProp.boolValue) {
                     return VRCFuryEditorUtils.Warn(
@@ -37,6 +36,16 @@ namespace VF.Feature {
                 }
                 return new VisualElement();
             }, includePuppetsProp));
+            
+            var includeBoolsProp = prop.FindPropertyRelative("includeBools");
+            content.Add(VRCFuryEditorUtils.Prop(includeBoolsProp, "Compress Bool Toggles"));
+            content.Add(VRCFuryEditorUtils.RefreshOnChange(() => {
+                if (includeBoolsProp.boolValue) {
+                    return VRCFuryEditorUtils.Warn(
+                        "Warning: Compressing bools often doesn't save much space, and can, in some rare cases, cause unusual sync issues with complex avatar systems.");
+                }
+                return new VisualElement();
+            }, includeBoolsProp));
 
             return content;
         }
