32e96c13dfe3d3c7ee80f79dbefd2e0d5eb393b4
Senky <117542830+SenkyDragon@users.noreply.github.com>
2024-10-18 08:10:00 -0500

    Convert Droppable into an action you can use in toggles

---
+++ com.vrcfury.vrcfury/Editor/VF/Actions/WorldDropActionBuilder.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VF.Feature.Base;
+using VF.Injector;
+using VF.Inspector;
+using VF.Model.StateAction;
+using VF.Service;
+using VF.Utils;
+
+namespace VF.Actions {
+    [FeatureTitle("World Drop")]
+    [FeatureHideTitleInEditor]
+    internal class WorldDropActionBuilder : ActionBuilder<WorldDropAction> {
+        [VFAutowired] [CanBeNull] private readonly WorldDropService worldDropService;
+        
+        public AnimationClip Build(WorldDropAction model, string actionName) {
+            var onClip = NewClip();
+            if (model.obj != null && worldDropService != null) {
+                var param = worldDropService.Add(model.obj, actionName);
+                // Doing this through an AAP will delay the drop by one frame, but that's exactly what we want
+                // because we want Turn On toggles to activate first (to reset the position) before the drop happens!
+                onClip.SetAap(param, 1);
+            }
+            return onClip;
+        }
+        
+        [FeatureEditor]
+        public static VisualElement Editor(SerializedProperty prop) {
+            var row = new VisualElement().Row();
+            row.Add(VRCFuryActionDrawer.Title("World Drop").FlexBasis(100));
+            row.Add(VRCFuryEditorUtils.Prop(prop.FindPropertyRelative("obj")).FlexGrow(1));
+            return row;
+        }
+    }
+}
---
+++ com.vrcfury.vrcfury/Runtime/VF/Model/StateAction/WorldDropAction.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+namespace VF.Model.StateAction {
+    [Serializable]
+    internal class WorldDropAction : Action {
+        public GameObject obj;
+    }
+}
