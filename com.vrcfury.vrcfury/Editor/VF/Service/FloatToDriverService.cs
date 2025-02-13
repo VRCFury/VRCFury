294431b57caf19e5d7ceef62475f8b4ac1cbbc4b
Senky <117542830+SenkyDragon@users.noreply.github.com>
2025-02-12 23:38:42 -0600

    Pack all drivers into one layer

--- com.vrcfury.vrcfury/Editor/VF/Service/FloatToDriverService.cs
+++ com.vrcfury.vrcfury/Editor/VF/Service/FloatToDriverService.cs
@@ -18,43 +18,84 @@ namespace VF.Service {
         private ControllerManager fx => controllers.GetFx();
 
         private BlendtreeMath math;
-        private readonly Dictionary<string, VFLayer> driveLayers = new Dictionary<string, VFLayer>();
+        private readonly List<DriveRequest> driveRequests = new List<DriveRequest>();
+
+        private class DriveRequest {
+            public string output;
+            public VFAFloat control;
+            public float? onValue;
+            public float? offValue;
+        }
+
+        [FeatureBuilderAction(FeatureOrder.DriveNonFloatTypes)]
+        public void Apply() {
+            if (driveRequests.Count == 0) return;
+            
+            var dbt = dbtLayerService.Create("FloatToDriverService - DBT");
+            var math = dbtLayerService.GetMath(dbt);
+
+            var layer = fx.NewLayer($"FloatToDriverService - Driver");
+            layer.NewState("Idle");
+            layer.SetNextOffset(1, 0);
+            
+            var threshold = 0.003f;
+
+            foreach (var outputGroup in driveRequests.GroupBy(req => req.output)) {
+                var output = outputGroup.Key;
+                var resolvedTrigger = fx.MakeAap($"FloatToDriverService - {output} (Resolved)");
+                var resolvedTriggerLastValue = 0;
+                var conditions = new List<(Motion, BlendtreeMath.VFAFloatBool)>();
+                var drivenLastFrame = fx.MakeAap($"FloatToDriverService - {output} (Driven Last Frame)");
+                var drivenLastFrameClip = VrcfObjectFactory.Create<AnimationClip>();
+                drivenLastFrameClip.SetAap(drivenLastFrame, 1);
+                var changedLastFrame = fx.MakeAap($"FloatToDriverService - {output} (Changed Last Frame)");
+                
+                void AddTrigger(float targetValue, BlendtreeMath.VFAFloatBool triggeredCondition) {
+                    var triggered = math.SetValueWithConditions($"FloatToDriverService - {output} = {targetValue} (Trigger)",
+                        (1f, triggeredCondition),
+                        (0f, null)
+                    );
+                    var resolvedTriggerValue = ++resolvedTriggerLastValue;
+                    var clip = VrcfObjectFactory.Create<AnimationClip>();
+                    clip.SetAap(changedLastFrame, 1);
+                    clip.SetAap(resolvedTrigger, resolvedTriggerValue);
+                    conditions.Add((clip, BlendtreeMath.GreaterThan(triggered, threshold)));
+
+                    var state = layer.NewState($"{output} = {targetValue}");
+                    state.TransitionsFromAny().When(resolvedTrigger.AsFloat().IsGreaterThan(resolvedTriggerValue - threshold)
+                        .And(resolvedTrigger.AsFloat().IsLessThan(resolvedTriggerValue + threshold)));
+                    state.Drives(output, targetValue);
+                    state.WithAnimation(drivenLastFrameClip);
+                }
+                
+                foreach (var req in outputGroup.Reverse()) {
+                    var input = req.control;
+                    var inputBuffered = math.Buffer(input);
+                    if (req.offValue.HasValue) {
+                        AddTrigger(req.offValue.Value, BlendtreeMath.GreaterThan(inputBuffered, threshold, true)
+                            .And(BlendtreeMath.LessThan(input, threshold)));
+                    }
+                    if (req.onValue.HasValue) {
+                        AddTrigger(req.onValue.Value, BlendtreeMath.LessThan(inputBuffered, threshold)
+                            .And(BlendtreeMath.GreaterThan(input, threshold, true)));
+                    }
+                }
+
+                var maintainClip = resolvedTrigger.MakeCopier(resolvedTrigger);
+                conditions.Add((null, BlendtreeMath.GreaterThan(changedLastFrame, threshold).Or(BlendtreeMath.LessThan(drivenLastFrame, threshold))));
+                conditions.Add((maintainClip, null));
+                math.SetValueWithConditions(conditions.ToArray());
+            }
+        }
 
         public VFAFloat Drive(string output, float? onValue, float? offValue) {
-            if (math == null) {
-                var dbt = dbtLayerService.Create("FloatToDriverService");
-                math = dbtLayerService.GetMath(dbt);
-            }
             var control = fx.NewFloat($"Drive {output} to {onValue}/{offValue}");
-            var buffer = math.Buffer(control);
-
-            if (!driveLayers.TryGetValue(output, out var layer)) {
-                layer = fx.NewLayer($"FloatToDriverService - {output}");
-                layer.NewState("Idle");
-                layer.SetNextOffset(1, 0);
-                driveLayers[output] = layer;
-            }
-
-            void MakeLastAnyTransitionFirst() {
-                var oldAny = layer.GetRawStateMachine().anyStateTransitions;
-                layer.GetRawStateMachine().anyStateTransitions = new[] { oldAny.Last() }
-                    .Concat(oldAny.Take(oldAny.Length - 1))
-                    .ToArray();
-            }
-
-            if (offValue.HasValue) {
-                var state = layer.NewState($"Set to {offValue}");
-                state.Drives(output, offValue.Value);
-                state.TransitionsFromAny().When(control.IsLessThanOrEquals(0).And(buffer.IsGreaterThan(0)));
-                MakeLastAnyTransitionFirst();
-            }
-            if (onValue.HasValue) {
-                var state = layer.NewState($"Set to {onValue}");
-                state.Drives(output, onValue.Value);
-                state.TransitionsFromAny().When(control.IsGreaterThan(0).And(buffer.IsLessThanOrEquals(0)));
-                MakeLastAnyTransitionFirst();
-            }
-
+            driveRequests.Add(new DriveRequest() {
+                output = output,
+                control = control,
+                onValue = onValue,
+                offValue = offValue
+            });
             return control;
         }
     }
