090a03c56181d15ff72da897cd3fcb400670796e
Senky <117542830+SenkyDragon@users.noreply.github.com>
2025-02-16 22:09:17 -0600

    Ensure drivers targeting synced parameters always run in local only mode

--- com.vrcfury.vrcfury/Editor/VF/Feature/Base/FeatureOrder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/Base/FeatureOrder.cs
@@ -78,6 +78,7 @@ namespace VF.Feature.Base {
         // Finalize Controllers
         UpgradeToVrcConstraints, // Needs to happen before any step starts looking at or cleaning up "invalid" animation bindings
         DisableSyncForAaps,
+        MakeAllSyncedDriversLocal,
         ParameterCompressor,
         FixGestureFxConflict, // Needs to run before DirectTreeOptimizer messes with FX parameters
         BlendShapeLinkFixAnimations, // Needs to run after most things are done messing with animations, since it'll make copies of the blendshape curves
---
+++ com.vrcfury.vrcfury/Editor/VF/Service/MakeAllSyncedDriversLocalService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using VF.Builder;
+using VF.Feature.Base;
+using VF.Injector;
+using VF.Utils;
+using VRC.SDK3.Avatars.Components;
+
+namespace VF.Service {
+    /**
+     * Parameter drivers targeting synced parameters should ALWAYS be set to "local only."
+     * If they're not, you can temporarily wind up with a desync on remote clients because vrc will
+     * assume the variable has been synced, then not re-send it until a second passes (meanwhile the remote
+     * may incorrectly be in the wrong state while this happens).
+     *
+     * VRCF makes all of its drivers with "local only" unchecked, under the assumption that this service
+     * will fix them afterward.
+     */
+    [VFService]
+    public class MakeAllSyncedDriversLocalService {
+        [VFAutowired] private readonly ControllersService controllers;
+        [VFAutowired] private readonly ParamsService paramsService;
+        private ParamManager paramz => paramsService.GetParams();
+
+        [FeatureBuilderAction(FeatureOrder.MakeAllSyncedDriversLocal)]
+        public void Apply() {
+            var syncedParams = paramz.GetRaw().parameters
+                .Where(param => param.IsNetworkSynced())
+                .Select(param => param.name)
+                .ToImmutableHashSet();
+
+            foreach (var layer in controllers.GetAllUsedControllers().SelectMany(c => c.GetLayers())) {
+                AnimatorIterator.ForEachBehaviourRW(layer, (b, add) => {
+                    if (!(b is VRCAvatarParameterDriver driver)) return true; // Not a driver, keep it
+                    if (driver.localOnly) return true; // Driver is already local only, keep it
+
+                    var synced = driver.parameters.Where(p => syncedParams.Contains(p.name)).ToList();
+                    var unsynced = driver.parameters.Where(p => !syncedParams.Contains(p.name)).ToList();
+
+                    if (synced.Count == 0) return true; // All params are unsynced, keep it as non-local-only
+                    if (unsynced.Count == 0) {
+                        // All params are synced, just make the whole thing local only
+                        driver.localOnly = true;
+                        return true;
+                    }
+
+                    var localDriver = (VRCAvatarParameterDriver)add(typeof(VRCAvatarParameterDriver));
+                    UnitySerializationUtils.CloneSerializable(driver, localDriver);
+                    localDriver.parameters = synced;
+                    driver.parameters = unsynced;
+                    return true;
+                });
+            }
+        }
+    }
+}
