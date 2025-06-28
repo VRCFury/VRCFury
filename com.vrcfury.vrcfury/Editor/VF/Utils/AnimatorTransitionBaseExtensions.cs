a94eef6ba4dd844ed6d2ac162eccd7c6da6c6b9b
dummics <domixartstm@gmail.com>
2025-06-29 01:10:49 +0200

    Fix: handle null transitions and empty conditions in RewriteConditions

--- com.vrcfury.vrcfury/Editor/VF/Utils/AnimatorTransitionBaseExtensions.cs
+++ com.vrcfury.vrcfury/Editor/VF/Utils/AnimatorTransitionBaseExtensions.cs
@@ -4,54 +4,72 @@ using System.Linq;
 using UnityEditor.Animations;
 using VF.Inspector;
 
-namespace VF.Utils {
-    internal static class AnimatorTransitionBaseExtensions {
+namespace VF.Utils
+{
+    internal static class AnimatorTransitionBaseExtensions
+    {
         /**
          * Updating conditions is expensive because it calls AnimatorController.OnInvalidateAnimatorController
          * So only do if it something actually changes.
          */
-        public static AnimatorTransitionBase[] RewriteConditions(this AnimatorTransitionBase transition, Func<AnimatorCondition, Rewritten> rewrite) {
+        public static AnimatorTransitionBase[] RewriteConditions(
+        this AnimatorTransitionBase transition,
+        Func<AnimatorCondition, Rewritten> rewrite)
+        {
             var updated = false;
-            var andOr = transition.conditions.SelectMany(condition => {
-                var rewritten = rewrite(condition);
-                if (rewritten.andOr.Length != 1 || rewritten.andOr[0].Length != 1 || !rewritten.andOr[0][0].Equals(condition)) {
-                    updated = true;
-                }
-                return rewritten.andOr;
-            }).ToArray();
-            if (!updated) {
-                return new[] { transition };
-            }
 
-            return andOr.CrossProduct()
-                .Select(and => {
+            /* 
+             * Guard-clauses for when a condition or a transition is null and not an empty array, 
+             * can be caused by a script generated .controller with broken transitions 
+             */
+             
+            if (transition == null)
+                return Array.Empty<AnimatorTransitionBase>();
+            var conds = transition.conditions ?? Array.Empty<AnimatorCondition>();
+
+            var andOr = conds.SelectMany(condition =>
+                {
+                    var rewritten = rewrite(condition);
+                    if (rewritten.andOr.Length != 1 || rewritten.andOr[0].Length != 1 || !rewritten.andOr[0][0].Equals(condition))
+                        updated = true;
+                    return rewritten.andOr;
+                }).ToArray();
+
+            if (!updated)
+                return new[] { transition };
+
+            return andOr.CrossProduct().Select(and =>
+                {
                     var clone = transition.Clone();
                     clone.conditions = and.ToArray();
                     return clone;
-                })
-                .ToArray();
+                }).ToArray();
         }
 
-        private static readonly AnimatorCondition True = new AnimatorCondition() {
+
+        private static readonly AnimatorCondition True = new AnimatorCondition()
+        {
             parameter = VFBlendTreeDirect.AlwaysOneParam,
             mode = AnimatorConditionMode.Greater,
             threshold = 0
         };
-        
-        private static readonly AnimatorCondition False = new AnimatorCondition() {
+
+        private static readonly AnimatorCondition False = new AnimatorCondition()
+        {
             parameter = VFBlendTreeDirect.AlwaysOneParam,
             mode = AnimatorConditionMode.Less,
             threshold = 0
         };
 
-        public class Rewritten {
-            public AnimatorCondition[][] andOr = {};
+        public class Rewritten
+        {
+            public AnimatorCondition[][] andOr = { };
 
             public static implicit operator Rewritten(AnimatorCondition[] and) => And(and);
             public static implicit operator Rewritten(AnimatorCondition single) => And(single);
             public static implicit operator Rewritten(bool single) => And(single ? True : False);
-            public static Rewritten Or(params AnimatorCondition[] or) => new Rewritten { andOr = new [] { or } };
-            public static Rewritten And(params AnimatorCondition[] and) => new Rewritten { andOr = and.Select(o => new [] { o }).ToArray() };
+            public static Rewritten Or(params AnimatorCondition[] or) => new Rewritten { andOr = new[] { or } };
+            public static Rewritten And(params AnimatorCondition[] and) => new Rewritten { andOr = and.Select(o => new[] { o }).ToArray() };
         }
     }
 }
