55bc38e718a23516ee4e81f8607af38219e0d32f
Senky <117542830+SenkyDragon@users.noreply.github.com>
2025-08-06 19:53:28 -0500

    Revert "Simplify wd autofix play mode saver"
    This reverts commit bc2c57789014290b7426e0592fedfccbf9adb50c.

--- com.vrcfury.vrcfury/Editor/VF/PlayMode/FixWriteDefaultsLater.cs
+++ com.vrcfury.vrcfury/Editor/VF/PlayMode/FixWriteDefaultsLater.cs
@@ -10,18 +10,26 @@ using VRC.SDK3.Avatars.Components;
 
 namespace VF.PlayMode {
     internal static class FixWriteDefaultsLater {
-        private static readonly List<Action> onEditMode = new List<Action>();
-
+        private const string Key = "vrcfFixWd";
+        
         [InitializeOnLoadMethod]
         private static void Init() {
             EditorApplication.playModeStateChanged += state => {
+                if (state == PlayModeStateChange.ExitingEditMode) {
+                    EditorPrefs.DeleteKey(Key);
+                }
+                
                 if (state == PlayModeStateChange.EnteredEditMode) {
-                    try {
-                        foreach (var c in onEditMode) {
-                            c();
+                    var data = GetData();
+                    EditorPrefs.DeleteKey(Key);
+                    foreach (var entry in data.entries) {
+                        var avatars = Resources.FindObjectsOfTypeAll<VRCAvatarDescriptor>()
+                            .Select(avatar => avatar.owner())
+                            .Where(obj => obj.scene.IsValid()) // Exclude prefabs
+                            .Where(obj => obj.name == entry.name);
+                        foreach (var avatar in avatars) {
+                            SaveNow(avatar, entry.auto);
                         }
-                    } finally {
-                        onEditMode.Clear();
                     }
                 }
             };
@@ -29,19 +37,19 @@ namespace VF.PlayMode {
 
         public static void Save(VFGameObject avatar, bool auto) {
             if (Application.isPlaying) {
-                onEditMode.Add(() => {
-                    SaveNow(avatar, auto);
-                });
+                SaveLater(avatar, auto);
             } else {
                 SaveNow(avatar, auto);
             }
         }
 
-        private static void SaveNow(VFGameObject avatar, bool auto) {
-            if (avatar == null) {
-                return;
-            }
+        private static void SaveLater(VFGameObject avatar, bool auto) {
+            var data = GetData();
+            data.entries.Add(new Entry() { auto = auto, name = avatar.name });
+            SetData(data);
+        }
 
+        private static void SaveNow(VFGameObject avatar, bool auto) {
             if (avatar.GetComponentsInSelfAndChildren<VRCFury>()
                 .SelectMany(v => v.GetAllFeatures())
                 .Any(f => f is FixWriteDefaults)) {
@@ -54,6 +62,32 @@ namespace VF.PlayMode {
                     ? FixWriteDefaults.FixWriteDefaultsMode.Auto
                     : FixWriteDefaults.FixWriteDefaultsMode.Disabled
             };
+        }
+
+        private static Data GetData() {
+            var text = EditorPrefs.GetString(Key);
+            if (string.IsNullOrEmpty(text)) return new Data();
+            try {
+                return JsonUtility.FromJson<Data>(text);
+            } catch (Exception) {
+                return new Data();
+            }
+        }
+
+        private static void SetData(Data data) {
+            var str = JsonUtility.ToJson(data);
+            EditorPrefs.SetString(Key, str);
+        }
+
+        [Serializable]
+        private class Data {
+            public List<Entry> entries = new List<Entry>();
+        }
+
+        [Serializable]
+        private class Entry {
+            public string name;
+            public bool auto;
         }
     }
 }
