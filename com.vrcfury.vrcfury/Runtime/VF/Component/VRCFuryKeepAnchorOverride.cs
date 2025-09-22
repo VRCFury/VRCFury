fc7df2b5266b2e96421252fa4143df37d861eac6
Purpzie <25022704+Purpzie@users.noreply.github.com>
2025-09-22 13:03:19 -0500

    fix error, add meta files

--- com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryKeepAnchorOverride.cs
+++ com.vrcfury.vrcfury/Runtime/VF/Component/VRCFuryKeepAnchorOverride.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 namespace VF.Component {
     [AddComponentMenu("VRCFury/Keep Anchor Override (VRCFury)")]
     internal class VRCFuryKeepAnchorOverride : VRCFuryComponent {
+        public bool isWorldDrop = false;
     }
 }
