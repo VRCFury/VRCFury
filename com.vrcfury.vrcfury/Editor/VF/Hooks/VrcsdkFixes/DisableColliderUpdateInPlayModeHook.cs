619e735e7119062a66560e8242ab0f5e875fe6c4
Senky <117542830+SenkyDragon@users.noreply.github.com>
2025-10-03 17:21:06 -0500

    Fix VRCSDK resetting finger collider transforms when showing them in play mode

---
+++ com.vrcfury.vrcfury/Editor/VF/Hooks/VrcsdkFixes/DisableColliderUpdateInPlayModeHook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using VF.Utils;
+
+namespace VF.Hooks.VrcsdkFixes {
+    /**
+     * The vrcsdk updates collider transforms whenever you focus the avatar descriptor, but this makes it look like
+     * global collider tricks aren't working. We can disable this updater while in play mode so that it shows the tricks
+     * correctly.
+     */
+    internal static class DisableColliderUpdateInPlayModeHook {
+        [InitializeOnLoadMethod]
+        private static void Init() {
+            HarmonyUtils.Patch(
+                typeof(DisableColliderUpdateInPlayModeHook),
+                nameof(Prefix),
+                "AvatarDescriptorEditor3",
+                "UpdateAutoColliders"
+            );
+        }
+
+        private static bool Prefix() {
+            if (Application.isPlaying) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
