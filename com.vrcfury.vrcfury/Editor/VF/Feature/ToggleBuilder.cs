cfa31c55942bb142c2c8ecd23231bb13ecf5470d
Senky <117542830+SenkyDragon@users.noreply.github.com>
2025-12-01 04:18:52 -0600

    Additionally apply toggles to resting state if they a non-static, as long as they don't loop

--- com.vrcfury.vrcfury/Editor/VF/Feature/ToggleBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/ToggleBuilder.cs
@@ -286,7 +286,8 @@ namespace VF.Feature {
 
             if (!loadedRestingClip) {
                 loadedRestingClip = true;
-                if (restingClip.IsStatic()) {
+                var restingMotionLoops = new AnimatorIterator.Clips().From(restingClip).Any(clip => clip.isLooping);
+                if (!restingMotionLoops) {
                     savedRestingClip = restingClip.EvaluateMotion(1);
                     allClipsService.AddAdditionalManagedClip(savedRestingClip);
                 }
