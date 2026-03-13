fe08751f5760aa8a3ec65915dc9ea95dccd5800b
Senky <117542830+SenkyDragon@users.noreply.github.com>
2026-03-12 19:14:42 -0500

    Ensure layer and state names in a controller are unique within their container

--- com.vrcfury.vrcfury/Editor/VF/Feature/Base/FeatureOrder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/Base/FeatureOrder.cs
@@ -106,6 +106,7 @@ namespace VF.Feature.Base {
         AdjustWriteDefaults, // Needs to be after TreeFlattening, since it can change whether or not a layer has a DBT
         FixEmptyMotions, // Needs to be after AdjustWriteDefaults, since it changes behaviour if a state is WD on or off
         UpgradeWrongParamTypes,
+        MakeControllerNamesUnique,
         FinalizeController,
 
         // Finalize Menus
