0b55c814651915708fa10646134cdcfcea24e9c2
Senky <117542830+SenkyDragon@users.noreply.github.com>
2026-04-22 23:39:26 -0500

    Rework upload hook

--- com.vrcfury.vrcfury/Editor-Worlds/Hooks/IsActuallyUploadingWorldHook.cs
+++ com.vrcfury.vrcfury/Editor-Worlds/Hooks/IsActuallyUploadingWorldHook.cs
@@ -1,22 +1,9 @@
-﻿using UnityEditor;
-using VRC.SDK3.Editor;
+﻿using UnityEngine;
 
 namespace VF.Hooks {
     internal static class IsActuallyUploadingWorldHook {
-        private static bool actuallyUploading = false;
         public static bool Get() {
-            return actuallyUploading;
-        }
-
-        [InitializeOnLoadMethod]
-        private static void Init() {
-            VRCSdkControlPanel.OnSdkPanelEnable += (_, _2) => {
-                if (VRCSdkControlPanel.TryGetBuilder<IVRCSdkWorldBuilderApi>(out var builder)) {
-                    builder.OnSdkBuildStart += (_3, _4) => actuallyUploading = true;
-                    builder.OnSdkBuildError += (_3, _4) => actuallyUploading = false;
-                    builder.OnSdkBuildFinish += (_3, _4) => actuallyUploading = false;
-                }
-            };
+            return !Application.isPlaying;
         }
     }
 }
--- com.vrcfury.vrcfury/Editor-Worlds/Hooks/WorldUploadHook.cs
+++ com.vrcfury.vrcfury/Editor-Worlds/Hooks/WorldUploadHook.cs
@@ -1,16 +1,19 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using VF.Features;
 using VF.Menu;
 
 namespace VF.Hooks {
-    internal class WorldUploadHook : VrcfWorldPreprocessor {
-        protected override int order => -10000;
+    internal class WorldUploadHook : IProcessSceneWithReport {
+        public int callbackOrder => -10000;
 
-        protected override void Process(Scene scene) {
+        public void OnProcessScene(Scene scene, BuildReport report) {
             if (Application.isPlaying && !PlayModeMenuItem.Get()) return;
             if (IsActuallyUploadingWorldHook.Get() && !UseInUploadMenuItem.Get()) return;
 
+            TmpFilePackage.Cleanup();
             BuildInjectUnityActions.Process(scene);
             BuildSps.Process(scene);
             BuildMarker.Process(scene);
